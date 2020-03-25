eebc1eb56f1e5012562060ac46defd7e5c2cc5e2
Elizabeth Paul <elizabethpaul@elizabeths-mbp.home>
2020-03-25 08:14:43 +0000

    hmk

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,34 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import org.w3c.dom.Attr;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
-}
+
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark(){
+        this.attractions = new ArrayList<Attraction>();
+        this.stalls = new ArrayList<Stall>();
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        ArrayList<IReviewed> reviews = new ArrayList<IReviewed>();
+        for (Attraction attraction : attractions) {
+            if (attraction instanceof IReviewed) {
+                reviews.add(attraction);
+            }
+        }
+
+            for (Stall stall : stalls) {
+                if (stall instanceof IReviewed) {
+                    reviews.add(stall);
+                }
+            }
+            return reviews;
+        }
+    }
